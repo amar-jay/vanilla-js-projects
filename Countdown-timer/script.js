b08b2c0ef9d3bb97d127c379a4c16d58c1f6ee23
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2021-10-23 13:34:51 +0300

    Trying...

---
+++ Countdown-timer/script.js
@@ -0,0 +1,14 @@
+// Initialization
+dayContainer = document.getElementById("day");
+hourContainer = document.getElementById("hour");
+minuteContainer = document.getElementById("minute");
+secondContainer = document.getElementById("second");
+
+console.log("DAY:", dayContainer);
+console.log("HOUR:", hourContainer);
+console.log("MINUTE:", minuteContainer);
+console.log("SECOND:", secondContainer);
+
+const current = new Date();
+
+console.log(current);
